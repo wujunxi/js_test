0eaee9728499fb27acdbd41ed0755fef35adacf3
xn066417 <wujunxi@xiaoniu66.com>
2017-10-09 14:07:03 +0800

    update

--- autoAnswer/index.js
+++ autoAnswer/index.js
@@ -3,36 +3,46 @@
 //     (id && id.indexOf('answer') != -1) ? console.log(id.match(/\d+$/g)[0] + ' ' + $e.val()): ''; 
 // })
 
-
-$('[type=hidden]').each(function(i, e) {
-    var $e = $(e);
-    var id = $e.attr('id');
-    var index, answer;
-    if (id && id.indexOf('answer') != -1) {
-        index = id.match(/\d+$/g)[0];
-        answer = $e.val();
-        var $p = $e.closest('#t' + index);
-        answer.split(',').forEach(function(item) {
-            var selectStr = '[name=q' + index + '][value=' + item + ']';
-            var $elem = $(selectStr, $p).prop('checked', true)
-        });
-    }
-})
-
-$.ajax({
-    cache: true,
-    type: "POST",
-    url: '/question/Post/Answer?token=636431455604115902',
-    data: $('#surveyform').serialize(), // 你的formid
-    async: false,
-    error: function(request) {
-        alert("Connection error");
-        ////console.log(request);
-    },
-    success: function(res) {
-        ////console.log(res);
-        if (res.status == "success") {
-            window.location.href = '/question/mobile/result';
+(function() {
+    // 复制答案
+    $('[type=hidden]').each(function(i, e) {
+        var $e = $(e);
+        var id = $e.attr('id');
+        var index, answer;
+        if (id && id.indexOf('answer') != -1) {
+            index = id.match(/\d+$/g)[0];
+            answer = $e.val();
+            var $p = $e.closest('#t' + index);
+            answer.split(',').forEach(function(item) {
+                var selectStr = '[name=q' + index + '][value=' + item + ']';
+                var $elem = $(selectStr, $p).prop('checked', true)
+            });
         }
-    }
-});
+    })
+    // 获取token
+    var token = '';
+    $('script').each(function(i, e) {
+        var str = $(e).html();
+        if (str) {
+            var temp = str.match(/token=(\d+)/);
+            token = temp ? temp[1] : '';
+        }
+    })
+    // 提交结果
+    $.ajax({
+        cache: true,
+        type: "POST",
+        url: '/question/Post/Answer?token='+token,
+        data: $('#surveyform').serialize(), 
+        async: false,
+        error: function(request) {
+            alert("Connection error");
+        },
+        success: function(res) {
+            if (res.status == "success") {
+                window.location.href = '/question/mobile/result';
+            }
+        }
+    });
+
+})();
